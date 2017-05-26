e1c55b0ca2154f02c5e074e21095fd809df04db5
Nazarii Muzychuk <muzychuk.n@gmail.com>
2017-05-26 20:26:59 +0300

    Update sorting tests

--- insertion-sort/src/test/java/com/nmuzychuk/InsertionSorterTest.java
+++ insertion-sort/src/test/java/com/nmuzychuk/InsertionSorterTest.java
@@ -1,10 +1,13 @@
 package com.nmuzychuk;
 
-import junit.framework.TestCase;
-
 import java.util.Arrays;
 
-public class InsertionSorterTest extends TestCase {
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class InsertionSorterTest {
+    @Test
     public void testEmptyArray() {
         int[] a1 = {};
         int[] a2 = a1.clone();
@@ -12,9 +15,10 @@ public class InsertionSorterTest extends TestCase {
         InsertionSorter.sort(a1);
         Arrays.sort(a2);
 
-        assert (Arrays.equals(a1, a2));
+        assertTrue(Arrays.equals(a1, a2));
     }
 
+    @Test
     public void testOneElementArray() {
         int[] a1 = {1};
         int[] a2 = a1.clone();
@@ -22,9 +26,10 @@ public class InsertionSorterTest extends TestCase {
         InsertionSorter.sort(a1);
         Arrays.sort(a2);
 
-        assert (Arrays.equals(a1, a2));
+        assertTrue(Arrays.equals(a1, a2));
     }
 
+    @Test
     public void testTwoElementsArray() {
         int[] a1 = {1, -1};
         int[] a2 = a1.clone();
@@ -32,9 +37,10 @@ public class InsertionSorterTest extends TestCase {
         InsertionSorter.sort(a1);
         Arrays.sort(a2);
 
-        assert (Arrays.equals(a1, a2));
+        assertTrue(Arrays.equals(a1, a2));
     }
 
+    @Test
     public void testFewElementsArray() {
         int[] a1 = {1, 0, -1};
         int[] a2 = a1.clone();
@@ -42,6 +48,6 @@ public class InsertionSorterTest extends TestCase {
         InsertionSorter.sort(a1);
         Arrays.sort(a2);
 
-        assert (Arrays.equals(a1, a2));
+        assertTrue(Arrays.equals(a1, a2));
     }
 }
